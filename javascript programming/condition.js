33300d8dfeb5086c4c15727b569e716c05c74c41
KRITHIGA <94514649+Krithi-28@users.noreply.github.com>
2024-04-30 14:26:42 +0530

    Add files via upload

---
+++ javascript programming/condition.js
@@ -0,0 +1,7 @@
+var weather='rain';
+if (weather=="rain"){
+    console.log('its raining today');
+}
+else{
+    console.log("its not raining");
+}
---
+++ javascript programming/weather/weather.js
@@ -0,0 +1,19 @@
+let weather='rain';
+switch(weather){
+    case 'rain':
+        window.alert('its rainy season');
+        break;
+    case 'sun':
+        window.alert("its summer season")
+        break;
+    case 'wind':
+        window.alert("its autumn season");
+        break;
+    case 'leaf':
+        window.alert("its spring season");
+        break;
+    case 'snow':
+        window.alert("its winter season");
+        break;
+    
+}
